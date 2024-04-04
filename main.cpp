cee5ef227ea255f586459c28a08f42d03a8cb138
BuildTools <unconfigured@null.spigotmc.org>
2024-04-04 22:56:55 +0300

    bababa

--- main.cpp
+++ main.cpp
@@ -4,30 +4,40 @@
 #include <map>
 #include <memory>
 #include <vector>
+#include <string>
+#include <sstream>
 
 
 using namespace std;
 
 class Character {
 protected:
-    const int healthPoints;
+    int healthPoints;
     const string name;
 
 public:
     Character(string &n, int HP) : name(n), healthPoints(HP){}
     virtual ~Character() = default;
 
-    friend ostream& operator<<(ostream& os, const Character& character){
 
-    }
 
-    string getName(){
+    virtual string getName(){
         return name;
     }
 
-    int getHP(){
+    virtual int getHP(){
         return healthPoints;
     }
+
+private:
+    virtual void takeDamage(int damage){
+        healthPoints -= damage;
+    }
+    virtual void heal(int healValue) {
+        healthPoints += healValue;
+    }
+
+
 
 
 };
@@ -44,22 +54,29 @@ public:
 };
 
 class Weapon : public PhysicalItem {
+private:
+    int damage;
 public:
-    Weapon(string &n, Character &character) : PhysicalItem(n, character, false)
+    Weapon(string &n, Character &character, int damage) : PhysicalItem(n, character, false), damage(damage)
     {
 
     }
 };
 
 class Potion : public PhysicalItem {
+private:
+    int healValue;
 public:
-    Potion(string &name, Character &owner) : PhysicalItem(name, owner, true)
+    Potion(string &name, Character &owner, int healValue) : PhysicalItem(name, owner, true), healValue(healValue)
     {
 
     }
 };
 
 class Spell : PhysicalItem {
+private:
+    vector<Character> allowedTargets;
+public:
     Spell(string &name, Character &owner) : PhysicalItem(name, owner, true)
     {
 
@@ -72,12 +89,8 @@ class Container{
 protected:
     vector<T> elems;
 public:
-    Container(){
-        cout << "Base container created"<<endl;
-    }
-    ~Container(){
-        cout << "Base container destroyed" << endl;
-    }
+    Container()= default;
+    ~Container()= default;
 };
 template<typename T>
 concept DerivedFromPhysicalItem = is_base_of<PhysicalItem, T>::value;
@@ -94,7 +107,6 @@ public:
     Container(int size){
         elements = std::move(map<string, unique_ptr<T>>());
         maxCapacity = size;
-        cout << "Container max capa created" << endl;
     }
 
     bool isFull(){
@@ -125,27 +137,40 @@ public:
     void removeItem(string & itemName){
         elements.erase(itemName);
     }
+    void resizeContainer(int size){
+        maxCapacity = size;
+    }
 
 
 };
 
+typedef Container<Weapon> Arsenal;
+typedef Container<Potion> MedicalBag;
+typedef Container<Spell> SpellBook;
+
 
 class WeaponUser : virtual public Character{
+protected:
+    Arsenal arsenal;
 public:
-    WeaponUser(string &n, int HP) : Character(n, HP){
+    WeaponUser(string &n, int HP) : Character(n, HP), arsenal(0){
 
     }
 };
 
 class PotionUser : virtual public Character{
+protected:
+    MedicalBag medicalBag;
 public:
-    PotionUser(string &n, int HP) : Character(n, HP){}
+    PotionUser(string &n, int HP) : Character(n, HP), medicalBag(0){}
 
 };
 
 class SpellUser : virtual public Character{
+protected:
+    SpellBook spellBook;
 public:
-    SpellUser(string &n, int HP) : Character(n, HP){}
+    SpellUser(string &n, int HP) : Character(n, HP), spellBook(0){}
 
 };
 
@@ -155,7 +180,11 @@ public:
     const int maxAllowedPotions = 5;
     Fighter(string &n, int HP) : Character(n, HP), PotionUser(n, HP), WeaponUser(n, HP){
         cout << "A new fighter came to town, " << name << endl;
+        arsenal.resizeContainer(maxAllowedWeapons);
+        medicalBag.resizeContainer(maxAllowedPotions);
     }
+
+
 
 
 };
@@ -167,6 +196,9 @@ public:
     const int maxAllowedSpells = 2;
     Archer(string &n, int HP) : Character(n, HP), PotionUser(n, HP), WeaponUser(n, HP), SpellUser(n, HP){
         cout << "A new archer came to town, " << name << endl;
+        arsenal.resizeContainer(maxAllowedWeapons);
+        medicalBag.resizeContainer(maxAllowedPotions);
+        spellBook.resizeContainer(maxAllowedSpells);
     }
 
 };
@@ -177,16 +209,54 @@ public:
     const int maxAllowedSpells = 10;
     Wizard(string &n, int HP) : Character(n, HP), PotionUser(n, HP), SpellUser(n, HP){
         cout << "A new wizard came to town, " << name << endl;
+        medicalBag.resizeContainer(maxAllowedPotions);
+        spellBook.resizeContainer(maxAllowedSpells);
     }
 
 };
 
+
+
 class Narrator {
 private:
-    string name;
+    string name = "Narrator";
+public:
+    Narrator() = default;
 };
 
 int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    map<string, Character> characters;
+    int n;
+    cin >> n;
+    string command;
+    for (int i = 0; i <= n; i++){
+        getline(cin, command);
+        if (command.empty()){
+            continue;
+        }
+        vector<string> words;
+        istringstream iss(command);
+        string word;
+        while (iss >> word) {
+            words.push_back(word);
+        }
+        if (words[0] == "Create") {
+            if (words[1] == "character"){
+                string type = words[2];
+                string name = words[3];
+                int initHP = stoi(words[4]);
+                if (type == "fighter"){
+                    characters.insert(pair <string, Character> (name, Fighter(name, initHP)));
+                } else if (type == "wizard") {
+                    characters.insert(pair <string, Character> (name, Wizard(name, initHP)));
+                } else if (type == "archer") {
+                    characters.insert(pair <string, Character> (name, Archer(name, initHP)));
+                }
 
+            }
+        }
+    }
     return 0;
 }
